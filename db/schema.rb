27690b7028c96ba72ed1be8f6e2bdf49dfb98496
Katy La Tour <klatour324@gmail.com>
2021-06-02 09:32:28 -0500

    Create migrations for teas and subscriptions. Update schema with new tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_220652) do
+ActiveRecord::Schema.define(version: 2021_06_01_231449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,28 @@ ActiveRecord::Schema.define(version: 2021_06_01_220652) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "subscriptions", force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.bigint "tea_id", null: false
+    t.string "title"
+    t.float "price"
+    t.boolean "status", default: true
+    t.string "frequency"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
+    t.index ["tea_id"], name: "index_subscriptions_on_tea_id"
+  end
+
+  create_table "teas", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.float "temperature"
+    t.string "brew_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "subscriptions", "customers"
+  add_foreign_key "subscriptions", "teas"
 end
