7402c002ea901adbda4d481b3ab3da2d4c83f342
Katy La Tour <klatour324@gmail.com>
2021-06-02 21:43:50 -0500

    Add model spec for subscription status and add model validations for subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,8 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :tea
+
+  validates_presence_of :title
+  validates_presence_of :price
+  validates_presence_of :frequency
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -5,4 +5,28 @@ RSpec.describe Subscription, type: :model do
     it { should  belong_to :tea }
     it { should  belong_to :customer }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :price }
+    it { should validate_presence_of :frequency }
+  end
+
+  describe 'subscription status' do
+    before :each do
+      @subscription = create(:subscription)
+    end
+
+    it 'can have an active status with a valid subscription' do
+      @subscription = create(:subscription, active: true)
+
+      expect(@subscription.active).to eq(true)
+    end
+
+    it 'can have a cancelled subscription status when subscription is no longer valid' do
+      @subscription = create(:subscription, active: false)
+
+      expect(@subscription.active).to eq(false)
+    end
+  end
 end
