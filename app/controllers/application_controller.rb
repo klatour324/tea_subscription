31c79720e3f3d9a0af998a3d21c1f70471ea3c2b
Katy La Tour <klatour324@gmail.com>
2021-06-03 02:35:55 -0500

    Fix typo for ActiveRecordNotFound error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::API
-  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def error(message = "Invalid request error. Please check the request.")
     render json: { error: message }, status: :bad_request
   end
 
-  # def record_not_found(exception)
-  #   render json: { error: exception.message }, status: :not_found
-  # end
+  def record_not_found(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
 end
