da067b3526b80147c514af0900fd0f1cc1bc69fe
Katy La Tour <klatour324@gmail.com>
2021-06-02 21:45:48 -0500

    Update factories typos and data info

--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
-    street_address { Faker::Add ress.street_name }
+    street_address { Faker::Address.street_name }
     city { Faker::Address.city }
     state { Faker::Address.state_abbr }
     zipcode { Faker::Address.zip_code }
