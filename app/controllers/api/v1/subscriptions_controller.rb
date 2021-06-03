2a0ed01ed0e18c81f82601c6accf0e65b0040257
Katy La Tour <klatour324@gmail.com>
2021-06-02 22:50:34 -0500

    Add subscriptions_controller with create action

---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::SubscriptionsController < ApplicationController
+  def create
+    subscription = Subscription.new(subscription_params)
+    if subscription.save
+      render json: SubscriptionSerializer.new(subscription), status: :created
+    else
+      error(subscription.errors.full_messages.to_sentence)
+    end
+  end
+
+  private
+
+  def subscription_params
+    params.permit(:title, :price, :frequency, :customer_id, :tea_id)
+  end
+end
