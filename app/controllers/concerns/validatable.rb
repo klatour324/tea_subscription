5f20c5352da7659e6674bb3594c576010b5a5ec6
Katy La Tour <klatour324@gmail.com>
2021-06-06 20:00:20 -0500

    Update invalid_params? method

--- app/controllers/concerns/validatable.rb
+++ app/controllers/concerns/validatable.rb
@@ -2,6 +2,6 @@ module Validatable
   extend ActiveSupport::Concern
 
   def invalid_params?
-    return true if params[:status].blank? || params[:id].nil?
+    return true if params[:status].nil? || params[:id].nil?
   end
 end
