a3e7cc42568a8a584746164c6cc8233073db5801
Katy La Tour <klatour324@gmail.com>
2021-06-06 20:02:15 -0500

    Refactor error methods into error_handlers module. Include module in ApplicationController. Add Rescue from ArgumentError

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,5 @@
 class ApplicationController < ActionController::API
+  include ErrorHandlers
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
-  def error(message = "Invalid request error. Please check the request.")
-    render json: { error: message }, status: :bad_request
-  end
-
-  def record_not_found(exception)
-    render json: { error: exception.message }, status: :not_found
-  end
+  rescue_from ArgumentError, with: :invalid_status
 end
